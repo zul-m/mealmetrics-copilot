10c6f23c5f2061d0eccf416f6bdbeaef43acd8a6
Zulfikar Muhamad <zul.m@icloud.com>
2023-08-28 17:20:04 +0800

    Build the server

---
+++ pages/api/server.js
@@ -0,0 +1,17 @@
+// create a server with express and make the default port 8080
+
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+}
+);
+
+app.listen(port, () => {
+    console.log('Server is running on port ${port}')
+}
+)
