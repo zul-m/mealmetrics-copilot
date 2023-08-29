2e386a2ac68a0c49a79de9dc37eef29b777074aa
Zulfikar Muhamad <zul.m@icloud.com>
2023-08-29 16:21:29 +0800

    Updated the completion function and response

--- pages/api/generateInfo.js
+++ pages/api/generateInfo.js
@@ -1,6 +1,6 @@
 // create a controller
 
-const { Configuration, OpenAIApi } = require('openai');
+const { Configuration, OpenAIApi } = require("openai");
 const { recipePrompt } = require("../../data/recipe.json");
 
 const config = new Configuration({
@@ -11,23 +11,21 @@ const openai = new OpenAIApi(config);
 
 const generateInfo = async (req, res) => {
     const { recipe } = req.body;
-    
-    try {
-        const response = await openai.createCompletion({
-            engine: 'davinci',
-            prompt: req.body.prompt,
-            maxTokens: 64,
-            temperature: 0.9,
-            topP: 1,
-            presencePenalty: 0,
-            frequencyPenalty: 0,
-            bestOf: 1,
-            n: 1,
-            stream: false,
-            stop: ['\n'],
-        });
 
-        res.status(200).json(response);
+    try {
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: `${recipePrompt}${recipe}` }],
+            maxTokens: 200,
+            temperature: 0,
+            n: 1,
+        });
+        const response = completion.data.choices[0].message.content;
+
+        return res.status(200).json({
+            success: true,
+            data: response,
+        });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
